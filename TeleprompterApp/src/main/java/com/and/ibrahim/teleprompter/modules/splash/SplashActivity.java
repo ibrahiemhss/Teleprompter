b3e04c4620d6ef8f70b376d2e7eb53a72d778147
ibrahiemhss <kekoreko3@gmail.com>
2018-11-29 02:47:37 +0200

    Update SplashActivity.java

--- TeleprompterApp/src/main/java/com/and/ibrahim/teleprompter/modules/splash/SplashActivity.java
+++ TeleprompterApp/src/main/java/com/and/ibrahim/teleprompter/modules/splash/SplashActivity.java
@@ -33,8 +33,8 @@ public class SplashActivity extends AppCompatActivity {
         ConsentSDK consentSDK = new ConsentSDK.Builder(this)
                 .addTestDeviceId("your device id from logcat") // Add your test device id "Remove addTestDeviceId on production!"
                 .addCustomLogTag("CUSTOM_TAG") // Add custom tag default: ID_LOG
-                .addPrivacyPolicy("https://sites.google.com/view/teleprompter-app/home/teleprompter") // Add your privacy policy url
-                .addPublisherId("pub-2142834117997426") // Add your admob publisher id
+                .addPrivacyPolicy("https://teleprompter-app/teleprompter") // Add your privacy policy url
+                .addPublisherId("pub-28787878989867676") // Add your admob publisher id
                 .build();
 
         // To check the consent and to move to MainActivity after everything is fine :).
